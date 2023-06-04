dde09a94a1efa5cd418dd57c1f45129273bc026e
koffermann <kerrian.offermann@snhu.edu>
2023-06-04 16:38:34 -0400

    Create new application API

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('./app_api/database/db');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+
+
 var apiRouter = require('./app_api/routes/index');
 
 var app = express();
