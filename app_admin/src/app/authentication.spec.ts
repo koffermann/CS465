f1ba172293faf28cf69add9e62550b67eebac873
koffermann <kerrian.offermann@snhu.edu>
2023-06-18 20:56:55 -0400

    Security updates and final project

---
+++ app_admin/src/app/authentication.spec.ts
@@ -0,0 +1,7 @@
+import { Authentication } from './services/authentication';
+
+describe('Authentication', () => {
+  it('should create an instance', () => {
+    expect(new Authentication()).toBeTruthy();
+  });
+});
